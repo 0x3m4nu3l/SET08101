94b09537a0c0a976f0b17db548a3a78a7bec022b
0x3m4nu3l <123465133+0x3m4nu3l@users.noreply.github.com>
2023-04-05 21:44:08 +0100

    Update contactus.js

--- js/contactus.js
+++ js/contactus.js
@@ -11,7 +11,7 @@ submit.addEventListener('submit',(e)=>{
     `;
 
     Email.send({
-        SecureToken : "a5b5a46a-70db-46f1-9b54-ddaf8e3dedcb", //add your token here
+        SecureToken : "cd70f837-5c43-4535-9646-428b42a1840f", //add your token here
         To : 'mario.lee.rossi@gmail.com', 
         From : '',
         Subject : "The Italian Way query",
