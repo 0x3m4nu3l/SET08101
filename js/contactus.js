be08801c54605775760fc047d92b63764b8dd6d8
0x3m4nu3l <123465133+0x3m4nu3l@users.noreply.github.com>
2023-04-05 20:54:17 +0100

    Add files via upload

---
+++ js/contactus.js
@@ -0,0 +1,22 @@
+const fname = document.getElementById('fname');
+const email = document.getElementById('email');
+const submit = document.getElementsByClassName('form-contact')[0];
+
+submit.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    let ebody = `
+    <h1>Full name: </h1>${fname.value}
+    <br>
+    <h1>Email: </h1>${email.value}
+    `;
+
+    Email.send({
+        SecureToken : "a5b5a46a-70db-46f1-9b54-ddaf8e3dedcb", //add your token here
+        To : 'mario.lee.rossi@gmail.com', 
+        From : "website visitor",
+        Subject : "The Italian Way query",
+        Body : ebody
+    }).then(
+      message => alert(message)
+    );
+});
